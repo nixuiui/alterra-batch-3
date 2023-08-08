eaec06b885028018fb75e5f46c6b8b49e125d41f
Niki <nikirahmadiwiharto@gmail.com>
2023-08-08 20:13:47 +0700

    Nambah Fitur

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo Fitur Pertama',
       theme: ThemeData(
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
